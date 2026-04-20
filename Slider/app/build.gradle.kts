f4ee904512feca8e6e2202507448622570fee157
yokkesan <lc.yteshima@gmail.com>
2026-04-20 13:28:08 +0900

    スライダー完成

--- Slider/app/build.gradle.kts
+++ Slider/app/build.gradle.kts
@@ -29,6 +29,21 @@ android {
             )
         }
     }
+
+    buildFeatures {
+
+        viewBinding = true
+
+    }
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_11
+
+        targetCompatibility = JavaVersion.VERSION_11
+
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- Slider/app/src/main/java/com/example/slider/MainActivity.kt
+++ Slider/app/src/main/java/com/example/slider/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.slider
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.slider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    // 表示データ
+    private val images = listOf(
+        R.drawable.dog,
+        R.drawable.horse,
+        R.drawable.kitten
+    )
+
+    private val texts = listOf(
+        "①",
+        "②",
+        "③"
+    )
+
+    private var index = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 初期表示
+        update()
+
+        // 右ボタン
+        binding.btnToRight.setOnClickListener {
+            index++
+            if (index >= images.size) index = 0
+            update()
         }
+
+        // 左ボタン
+        binding.btnToLeft.setOnClickListener {
+            index--
+            if (index < 0) index = images.size - 1
+            update()
+        }
+    }
+
+    private fun update() {
+        binding.imageView.setImageResource(images[index])
+        binding.textView3.text = texts[index]
     }
 }
